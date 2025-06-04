01305c4441012556123369aab8b170c2f062f5e6
ECcentricOG <umeshunde003@gmail.com>
2025-06-04 18:17:41 +0530

    next permutation and two sum solved

---
+++ Array.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+
+public class Array{
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> tracker = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            tracker.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(tracker.containsKey(target - nums[i]) && tracker.get(target - nums[i]) != i)
+                return new int[] {i, tracker.get(target - nums[i])};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void nextPermutation(int[] nums) {
+        int pt = -1;
+        for(int i = nums.length - 2; i >= 0; i--) {
+            if(nums[i]  < nums[i + 1]) {
+                pt = i;
+                break;
+            }
+        }
+        if(pt == -1) {
+            reverseArray(nums, 0);
+            return;
+        }
+        for(int i = nums.length - 1; i > pt; i--) {
+            if(nums[i] > nums[pt]) {
+                int temp = nums[i];
+                nums[i] = nums[pt];
+                nums[pt] = temp;
+                break;
+            }
+        }
+        reverseArray(nums, pt + 1);
+    }
+
+    public static void reverseArray(int[] arr, int start) {
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+
---
+++ Solution.java
@@ -0,0 +1,7 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        Array.twoSum(arr, 7);
+    }
+}
