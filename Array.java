c58a9d57aecfc66064b143e11971878ef4452303
ECcentricOG <umeshunde003@gmail.com>
2025-06-09 21:44:44 +0530

    attempt to solve but failed

--- Array.java
+++ Array.java
@@ -140,5 +140,26 @@ public class Array{
         }
         return ans;
     }
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] post = nums;
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i - 1] * nums[i];
+        }
+        for (int i = post.length - 2; i >= 0; i--) {
+            nums[i] = nums[i] * nums[i + 1];
+        }
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < ans.length; i++) {
+            if(i == 0) {
+                ans[i] = post[i + 1];
+            } else if(i == post.length - 1) {
+                ans[i] = nums[i - 1];
+            } else {
+                ans[i] = post[i + 1] * nums[i - 1];
+            }
+        }
+        return ans;
+    }
 }
 
