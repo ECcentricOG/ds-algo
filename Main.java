e57184c4280a92a398da4290a3e41a263203da39
ECcentricOG <umeshunde003@gmail.com>
2025-08-30 18:15:03 +0530

    basic recusion problem for function is created

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ import recursion.Basic;
 public class Main {
 
     public static void main(String[] args) {
-        Basic.printOneToN(5);
+        int[] arr = new int[]{1,2,3,4,5};
+        Basic.reverseArray(arr, 0, arr.length - 1);
+        for(var a : arr) {
+            System.out.println(a);
+        }
     }
 }
--- recursion/Basic.java
+++ recursion/Basic.java
@@ -7,4 +7,22 @@ public class Basic {
         printOneToN(num - 1);
         System.out.println(num);
     }
+
+    public static int sumOfN(int num) {
+        if(num == 1) return 1;
+        int sum = num + sumOfN(num - 1);
+        return sum;
+    }
+
+    public static void reverseArray(int[] arr, int left, int right) {
+        if(left >= right) return;
+        arr[left] = arr[left] ^ arr[right];
+        arr[right] = arr[left] ^ arr[right];
+        arr[left] = arr[left] ^ arr[right];
+        reverseArray(arr, left + 1, right - 1);
+    }
+
+    public static void fibonacciSeries(int n) {
+
+    }
 }
